0662a39e123e64924ae71241a8d3b7696e24da44
Milos Milutinovic <milos.milutinovic@live.com>
2015-11-10 20:51:44 +0100

    Removed unnecessary files

--- application/views/install.php
+++ application/views/install.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <p class="bg-warning">Before proceeding check that database parameters are set correctly in <code>application/config/database.php</code> dbdriver will have to be set to mysqli or else the installation will have to be done manually.</p>
                     <p class="bg-info">Default username is <code>administrator</code> and password <code>password</code></p>
                     <p class="bg-danger">Make sure to change the password after installation.</p>
-                    <p class="bg-danger">After installation is complete, remove or disable access to <code>application/controllers/Install.php</code>, <code>application/sql</code>.</p>
+                    <p class="bg-danger">After installation is complete, remove or disable access to <code>application/controllers/Install.php</code> and <code>application/sql</code>.</p>
                     <p class="bg-danger">Do not upload <code>application/tests</code> to the production server</p>
                     <p><a href="<?= $urls['install'];?>"><button class="btn btn-primary">Install</button></a></p>
 
